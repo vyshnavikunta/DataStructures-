2eaae86c73e3f7aaae0d45997a8ba5c28888dd51
Kunta Vyshnavi <135843758+vyshnavikunta@users.noreply.github.com>
2024-10-04 15:37:57 +0530

    Create Sort 0s, 1s and 2s.java

---
+++ Arrays/Sort 0s, 1s and 2s.java
@@ -0,0 +1,26 @@
+/*
+Given an array arr containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Examples:
+
+Input: arr[]= [0, 2, 1, 2, 0]
+Output: 0 0 1 2 2
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Input: arr[] = [0, 1, 0]
+Output: 0 0 1
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= arr.size() <= 106
+0 <= arr[i] <= 2
+*/
+
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(ArrayList<Integer> arr) {
+        // code here
+        Collections.sort(arr);
+    }
+}
